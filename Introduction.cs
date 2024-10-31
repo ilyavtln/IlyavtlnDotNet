abdcb33057cf3dd9a17ba3c5a3c0f70df7d0fcdb
owtf086 <owtf086@gmail.com>
2024-10-31 22:35:54 +0300

    init

---
+++ Introduction.cs
@@ -0,0 +1,10 @@
+﻿namespace IlyavtlnDotNet;
+
+public class Introduction
+{
+    private readonly string _gitLink = "https://github.com/ilyavtln/IlyavtlnDotNet";
+    
+    public string CreatorName => "Ilya Vatlin";
+    public string CreatorEmail => "ilyavtln@gmail.com";
+    public string CreatorGit => _gitLink;
+}
